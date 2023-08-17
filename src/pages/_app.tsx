1c741821306ab6f81417ff805f572c4da12ecfd3
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-08-18 00:33:23 +0900

    feat: apply Loading component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,22 @@
 import type { AppProps } from 'next/app';
 
+import { AnimatePresence } from 'framer-motion';
+import 'katex/dist/katex.min.css';
+import 'prismjs/themes/prism-coy.css';
+import 'react-notion-x/src/styles.css';
+
 import Fonts from '@/components/app/Fonts';
+import Loading from '@/components/app/Loading';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
-    <Fonts>
-      <Component {...pageProps} key={router.pathname} />
-    </Fonts>
+    <AnimatePresence mode='wait' initial={false}>
+      <Loading>
+        <Fonts>
+          <Component {...pageProps} key={router.pathname} />
+        </Fonts>
+      </Loading>
+    </AnimatePresence>
   );
 }
